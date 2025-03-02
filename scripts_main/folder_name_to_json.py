cee6a8f9c57bbec2ce4069ea6f6fa519d6e4b1be
toxichome-whoami <toxic.home.whoami@hotmail.com>
2025-03-02 13:33:14 +0600

    update

--- scripts_main/folder_name_to_json.py
+++ scripts_main/folder_name_to_json.py
@@ -1,18 +1,18 @@
 import json
 import os
 
-def create_zip_list_json():
+def create_zip_list_json(path):
     # Find all .zip files in the current directory
-    zip_files = [f for f in os.listdir() if f.endswith(".zip")]
+    zip_files = [f for f in os.listdir(path) if f.endswith(".zip")]
 
     # Create JSON structure
-    zip_data = {"files": zip_files}
+    zip_data = {"count": len(zip_files), "files": zip_files}
 
     # Save to data.json
-    with open("data.json", "w") as json_file:
+    with open(os.path.join(path, "data.json"), "w") as json_file:
         json.dump(zip_data, json_file, indent=4)
 
-    print("Created data.json with all .zip file names.")
+    print("Created data.json with all .zip file names in the given path.")
 
 # Run the function
-create_zip_list_json()
+create_zip_list_json(r"D:\Python\Git repos\manifest_files\manifest_files")
